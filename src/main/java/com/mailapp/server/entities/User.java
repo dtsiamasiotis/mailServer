27845ec1575550bcfaff961d92305e24d1003719
Dimitris Tsiamasiotis <dtsiamasiotis@gmail.com>
2020-06-27 19:20:00 +0300

    initial commit

---
+++ src/main/java/com/mailapp/server/entities/User.java
@@ -0,0 +1,25 @@
+package com.mailapp.server.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String password;
+
+    protected User(){}
+}
